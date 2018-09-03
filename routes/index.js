689b106e441f1085f0a65594a432af6ee5f4f487
Oddgeir <doddip123@gmail.com>
2018-09-03 23:25:11 +0000

    Updated index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    console.log('asdf')
+  res.render('index', { title: 'Rock-In' });
 });
 
 module.exports = router;
