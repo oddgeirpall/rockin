55ec1649db1c0eedaf378bdbee2543449f51c8a3
Oddgeir <doddip123@gmail.com>
2018-09-04 00:51:04 +0000

    Updated member info and pictures. Changed banner

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     console.log('asdf')
-  res.render('index', { title: 'Rock-In' });
+  res.render('index', { title: 'Plokk-In' });
 });
 
 module.exports = router;
